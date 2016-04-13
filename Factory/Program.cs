0d0f12198d15ef387bd74c704f3acc2b3749e700
YuraChubenets <u.chubenets@gmail.com>
2016-04-13 23:08:25 +0300

    add  factory method

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -10,6 +10,37 @@ namespace Factory
     {
         static void Main(string[] args)
         {
+
+            Creator creator = new ConcreteCreatorA();
+
+            Product prod = creator.FactoryMethod();
+            Console.WriteLine(prod.GetType());
+            Console.ReadKey();
+
         }
+    }
+
+    abstract class Product
+    { }
+
+    class ConcreteProductA : Product
+    { }
+
+    class ConcreteProductB : Product
+    { }
+
+    abstract class Creator
+    {
+        public abstract Product FactoryMethod();
+    }
+
+    class ConcreteCreatorA : Creator
+    {
+        public override Product FactoryMethod() { return new ConcreteProductA(); }
+    }
+
+    class ConcreteCreatorB : Creator
+    {
+        public override Product FactoryMethod() { return new ConcreteProductB(); }
     }
 }
