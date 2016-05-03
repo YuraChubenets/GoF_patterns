c7abaed8b6ccaa983c5c2e79f5c524ac32605ca0
YuraChubenets <u.chubenets@gmail.com>
2016-05-03 21:42:06 +0300

    feature  Decorator

---
+++ Decorator/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pizza pizza1 = new ItalianPizza();
+            pizza1 = new TomatoPizza(pizza1); // итальянская пицца с томатами
+            Console.WriteLine("Название: {0}", pizza1.Name);
+            Console.WriteLine("Цена: {0}", pizza1.GetCost());
+
+            Pizza pizza2 = new ItalianPizza();
+            pizza2 = new CheesePizza(pizza2);// итальянская пиццы с сыром
+            Console.WriteLine("Название: {0}", pizza2.Name);
+            Console.WriteLine("Цена: {0}", pizza2.GetCost());
+
+            Pizza pizza3 = new BulgerianPizza();
+            pizza3 = new TomatoPizza(pizza3);
+            pizza3 = new CheesePizza(pizza3);// болгарская пиццы с томатами и сыром
+            Console.WriteLine("Название: {0}", pizza3.Name);
+            Console.WriteLine("Цена: {0}", pizza3.GetCost());
+
+            Console.ReadLine();
+        }
+    }
+
+    // Когда следует использовать декораторы?
+    //
+    //  Когда надо динамически добавлять к объекту новые функциональные возможности.
+    //При этом данные возможности могут быть сняты с объекта
+    //
+    // Когда применение наследования неприемлемо. 
+    // Например, если нам надо определить множество различных функциональностей и 
+    //для каждой функциональности наследовать отдельный класс, то структура классов может очень сильно разрастись.
+    //Еще больше она может разрастись, если нам необходимо создать классы,
+    //реализующие все возможные сочетания добавляемых функциональностей.
+
+    abstract class Pizza
+    {
+        public Pizza(string n)
+        {
+            this.Name = n;
+        }
+        public string Name { get; protected set; }
+        public abstract int GetCost();
+    }
+
+    class ItalianPizza : Pizza
+    {
+        public ItalianPizza() : base("Итальянская пицца")
+        { }
+        public override int GetCost()
+        {
+            return 10;
+        }
+    }
+    class BulgerianPizza : Pizza
+    {
+        public BulgerianPizza()
+            : base("Болгарская пицца")
+        { }
+        public override int GetCost()
+        {
+            return 8;
+        }
+    }
+
+    abstract class PizzaDecorator : Pizza
+    {
+        protected Pizza pizza;
+        public PizzaDecorator(string n, Pizza pizza) : base(n)
+        {
+            this.pizza = pizza;
+        }
+    }
+
+    class TomatoPizza : PizzaDecorator
+    {
+        public TomatoPizza(Pizza p)
+            : base(p.Name + ", с томатами", p)
+        { }
+
+        public override int GetCost()
+        {
+            return pizza.GetCost() + 3;
+        }
+    }
+
+    class CheesePizza : PizzaDecorator
+    {
+        public CheesePizza(Pizza p)
+            : base(p.Name + ", с сыром", p)
+        { }
+
+        public override int GetCost()
+        {
+            return pizza.GetCost() + 5;
+        }
+    }
+
+   
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Library library = new Library();
+            Reader reader = new Reader();
+            reader.SeeBooks(library);
+
+            Console.Read();
+        }
+    }
+
+    //Когда использовать итераторы?
+
+    //Когда необходимо осуществить обход объекта без раскрытия его внутренней структуры
+
+    //Когда имеется набор составных объектов, и надо обеспечить единый интерфейс для их перебора
+
+    //Когда необходимо предоставить несколько альтернативных вариантов перебора одного и того же объекта
+
+
+    class Reader
+    {
+        public void SeeBooks(Library library)
+        {
+            IBookIterator iterator = library.CreateNumerator();
+            while (iterator.HasNext())
+            {
+                Book book = iterator.Next();
+                Console.WriteLine(book.Name);
+            }
+        }
+    }
+
+    interface IBookIterator
+    {
+        bool HasNext();
+        Book Next();
+    }
+    interface IBookNumerable
+    {
+        IBookIterator CreateNumerator();
+        int Count { get; }
+        Book this[int index] { get; }
+    }
+    class Book
+    {
+        public string Name { get; set; }
+    }
+
+    class Library : IBookNumerable
+    {
+        private Book[] books;
+        public Library()
+        {
+            books = new Book[]
+            {
+            new Book{Name="Война и мир"},
+            new Book {Name="Отцы и дети"},
+            new Book {Name="Вишневый сад"}
+            };
+        }
+        public int Count
+        {
+            get { return books.Length; }
+        }
+
+        public Book this[int index]
+        {
+            get { return books[index]; }
+        }
+        public IBookIterator CreateNumerator()
+        {
+            return new LibraryNumerator(this);
+        }
+    }
+    class LibraryNumerator : IBookIterator
+    {
+        IBookNumerable aggregate;
+        int index = 0;
+        public LibraryNumerator(IBookNumerable a)
+        {
+            aggregate = a;
+        }
+        public bool HasNext()
+        {
+            return index < aggregate.Count;
+        }
+
+        public Book Next()
+        {
+            return aggregate[index++];
+        }
+    }
+}
